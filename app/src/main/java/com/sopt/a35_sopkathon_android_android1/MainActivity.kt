3fc8929c6ced0ffb72c26594fbe6ba5ba586361e
SYAAINN <alswo08319@gmail.com>
2024-11-23 21:54:58 +0900

    init

---
+++ app/src/main/java/com/sopt/a35_sopkathon_android_android1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sopt.a35_sopkathon_android_android1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.a35_sopkathon_android_android1.ui.theme._35SOPKATHONANDROIDandroid1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _35SOPKATHONANDROIDandroid1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _35SOPKATHONANDROIDandroid1Theme {
+        Greeting("Android")
+    }
+}
