827b33c1c2423abde4b6c2f5a1a3e62624f3ee04
MinseoSong <mins041600@naver.com>
2024-11-24 04:51:06 +0900

    [feat] 랭킹 파트별 뷰 구현 완료

---
+++ app/src/main/java/com/sopt/a35_sopkathon_android_android1/presentation/minseo/RankingAllScreen.kt
@@ -0,0 +1,14 @@
+package com.sopt.a35_sopkathon_android_android1.presentation.minseo
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RankingAllScreen(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = "전체"
+    )
+}
